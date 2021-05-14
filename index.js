b308cc7dcb45de80156ab889cda4b8efaa8ef3a0
Abdullah Mamun <mamun098@gmail.com>
2021-05-15 09:54:30 +1000

    env added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 // const { json } = require('body-parser');
 const app = express();
-const PORT = 3005;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
